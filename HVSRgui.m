a4dda3df9eb1240441254ba4bb5b671a84c9619b
Aleksandar Mihaylov <amihaylov@ssenginuity.com>
2016-12-10 22:42:36 -0500

    Test changes

--- HVSRgui.m
+++ HVSRgui.m
@@ -1,9 +1,10 @@
 function HVSRgui
-    global Fs Ts frame_size PathName traffic_duration traffic_threshold
-    traffic_duration = 13;
-    traffic_threshold = 0.5;
+    global Fs Ts frame_size PathName frame_diff overlap
+    frame_diff = 0.1;
     frame_size = 1024;
-    fig = figure();
+    overlap = 0.5;
+    
+    fig = figure(3);
     set(fig, 'ToolBar', 'none');
     tab_group = uitabgroup('Parent', fig, 'SelectionChangedFcn', @tab_changed);
     
@@ -25,42 +26,41 @@ function HVSRgui
     
     createHVSRTab(tab_group);
 end
+
 function createHVSRTab(tab_group)
     global frame_size
     window = hann(frame_size+1);
     userdata.window = repmat(window(1:end-1), 1, 3);
-    hvsr_tab = uitab('Parent',tab_group,'Title', 'HVSR');
-    userdata.ax_low = subplot(2,1,1, 'Parent', hvsr_tab,'XScale','log'); cla
-    userdata.ax_high = subplot(2,1,2, 'Parent', hvsr_tab,'XScale','log'); cla
+    hvsr_tab = uitab('Parent', tab_group, 'Title', 'HVSR');
+    userdata.ax_low = subplot(2, 1, 1, 'Parent', hvsr_tab,'XScale','log'); cla
+    userdata.ax_high = subplot(2, 1, 2, 'Parent', hvsr_tab,'XScale','log'); cla
     set(hvsr_tab, 'UserData', userdata);
     export_button = uicontrol('Style', 'pushbutton', ...
         'String', 'Export', 'Parent', hvsr_tab, ...
-        'Callback', @exportHVSR);
+        'Callback', @exportHVSR); %#ok<NASGU>
 end
+
 function exportHVSR(hObject, eventdata)
-    global HVSR
-%     hvsr_tab = get(hObject, 'Parent');
-%     HVSR_figure = hvsr_tab.Parent.Parent;
-%     HVSR = hvsr_tab.UserData.HVSR;
+    global HVSR %#ok<NUSED>
     [FileName,PathName_save] = uiputfile('*.mat', 'Save HVSR Results');
     if(FileName == 0)
         return;
     end
-
     save(fullfile(PathName_save, FileName), 'HVSR');
     fig = hObject.Parent.Parent.Parent;
     print(fig, strcat(PathName_save, FileName(1:end-4), '.png'), '-dpng', '-r0');
     savefig(fig,strcat(PathName_save, FileName(1:end-4)),'compact');
 end
+
 function tab_changed(hObject, eventdata)
-    global Fs frame_size HVSR
+    global Fs frame_size HVSR num_chans
     if(~strcmp(eventdata.NewValue.Title, 'HVSR'))
         return
     end
+    
     HVSR = struct;
     hvsr_tab = eventdata.NewValue;
-    userdata = get(hvsr_tab, 'UserData');
-    window = userdata.window;
+    window = hvsr_tab.UserData.window;
     
     HVSR_H =[];
     HVSR_L =[];
@@ -70,14 +70,20 @@ function tab_changed(hObject, eventdata)
         if(~strcmp(eventdata.NewValue.Title, 'HVSR'))
             break;
         end
-        DataStruct = tab.UserData;
-        if(~DataStruct.UseFile)
-            continue;
+        chan_panels = tab.UserData;
+        for k = 1:1:length(chan_panels)
+            usefile = chan_panels(k).Children(1);
+            userdata = usefile.UserData;
+            if(usefile.Value)
+                H = calculateHVSR(userdata.Data, userdata.High, window);
+                L = calculateHVSR(userdata.Data, userdata.Low, window);
+                HVSR_H = [HVSR_H H]; %#ok<AGROW>
+                HVSR_L = [HVSR_L L]; %#ok<AGROW>
+            else
+                HVSR_H = [HVSR_H zeros(size(HVSR_H,1))]; %#ok<AGROW>
+                HVSR_L = [HVSR_L zeros(size(HVSR_H,1))]; %#ok<AGROW>
+            end
         end
-        DataStruct.HVSR_H = calculateHVSR(DataStruct.Data, DataStruct.frames_High, window);
-        DataStruct.HVSR_L = calculateHVSR(DataStruct.Data, DataStruct.frames_Low, window);
-        HVSR_H = [HVSR_H DataStruct.HVSR_H]; %#ok<AGROW>
-        HVSR_L = [HVSR_L DataStruct.HVSR_L]; %#ok<AGROW>
     end
     
     HmeanHVSR = zeros(frame_size/2, 2);
@@ -92,8 +98,8 @@ function tab_changed(hObject, eventdata)
     ax_high = hvsr_tab.UserData.ax_high;
     axes(ax_high); cla; hold on
     for f=1:1:2
-        subsetH = HVSR_H(:,:,f:2:end);
-        subsetL = HVSR_L(:,:,f:2:end);
+        subsetH = HVSR_H(:,f:2:end);
+        subsetL = HVSR_L(:,f:2:end);
         HmeanHVSR(:,f) = mean(reshape(subsetH, frame_size/2,2*numel(subsetH)/frame_size), 2);
         HstdHVSR(:,f) = std(reshape(subsetH, frame_size/2,2*numel(subsetH)/frame_size), 1, 2);
         LmeanHVSR(:,f) = mean(reshape(subsetL, frame_size/2,2*numel(subsetL)/frame_size), 2);
@@ -120,10 +126,10 @@ function tab_changed(hObject, eventdata)
         grid on; title('High')
         axis tight
     end
-%     set(hvsr_tab, 'UserData', userdata); 
 end
+
 function createNewTab(file, tab_group)
-    global Ts Fs frame_size PathName traffic_duration traffic_threshold
+    global Ts Fs frame_size PathName num_chans frame_diff overlap
     tab = uitab('Parent', tab_group, 'Title', file);
     matfile = strcat(PathName, file);
     load(matfile);
@@ -135,102 +141,99 @@ function createNewTab(file, tab_group)
         Fs = 1/Ts;
     end
     data = D(:,2:end);
-    vector_data = sqrt(data(:,1).^2+data(:,2).^2+data(:,3).^2);
-    DataStruct.t = t;
-    DataStruct.Data = data;
-    DataStruct.Vector = vector_data;
-    DataStruct.traffic_duration = traffic_duration;
-    DataStruct.traffic_threshold= traffic_threshold;
+    num_chans = size(data,2)/3;
+    vector_data = sqrt(data(:,1:3:end).^2+data(:,2:3:end).^2+data(:,3:3:end).^2);
     
-    threshold_slider = uicontrol('Style', 'slider', ...
-        'Min', 0, 'Max', 1, 'Value', DataStruct.traffic_threshold, ...
-        'Position', [20 20 300 20], 'String', 'Threshold', ...
-        'SliderStep',[0.05 0.1], ...
-        'Callback', @parameter_changed, 'Parent', tab);
-    threshold_readout = uicontrol('Style','text','String', ...
-        ['Threshold:', num2str(DataStruct.traffic_threshold)], ...
-        'Position', [320 20 100 20], 'Parent', tab);
-    wind_size_slider = uicontrol('Style', 'slider', ...
-        'Min', 1, 'Max', 25, 'SliderStep', [1 1]/24, ...
-        'Value', DataStruct.traffic_duration, ...
-        'Position', [20 40 300 20], 'String', 'Duration', ...
-        'Callback', @parameter_changed, 'Parent', tab);
-    wind_size_readout = uicontrol('Style','text','String', ...
-        ['Duration:', num2str(DataStruct.traffic_duration)], ...
-        'Position', [320 40 100 20], 'Parent', tab);
-    use_file_check = uicontrol('Style', 'checkbox', ...
-        'Value', 1, 'String', 'Use This File', 'Parent', tab, ...
-        'Position', [20 60 60 20], 'Callback', @checked_changed);
-    DataStruct.UseFile = use_file_check.Value;
-    
-    subplot(2,1,1, 'Parent', tab)
-    plot(t,vector_data)
-    axis tight; hold on
-    parameter_changed(wind_size_slider);
-    
-    function checked_changed(hObject, eventdata, handles)
-        parent = get(hObject, 'Parent');
-        userdata = get(parent, 'UserData');
-        userdata.UseFile = get(hObject, 'Value');
-        set(parent, 'UserData', userdata);
+    for kk=1:1:num_chans
+        panels(kk) = uipanel('Position', [0 1-kk/num_chans 0.1 1/num_chans], ...
+            'Tag', num2str(kk), 'Parent', tab);         %#ok<AGROW>
+        threshold_slider = uicontrol('Style', 'slider', ...
+            'Min', 0, 'Max', 1, 'Value', 0.5, ...
+            'Units', 'normalized', 'Position', [0 0.1 0.45 0.8], ...
+            'String', 'Threshold', 'SliderStep',[0.05 0.1], ...
+            'Callback', @parameter_changed, 'Parent', panels(kk)); %#ok<NASGU>
+        wind_size_slider = uicontrol('Style', 'slider', ...
+            'Min', 1, 'Max', Fs*15, 'Value', Fs*7, ...
+            'Units', 'normalized', 'Position', [0.55 0.1 0.45 0.8], ...
+            'String', 'Duration', 'SliderStep', Ts*[10 100], ...
+            'Callback', @parameter_changed, 'Parent', panels(kk));
+        
+        threshold_readout = uicontrol('Style','text', 'Parent', panels(kk), ...
+            'Units', 'normalized', 'Position', [0 0.9 0.45 0.1]); %#ok<NASGU>
+        wind_size_readout = uicontrol('Style','text', 'Parent', panels(kk), ...
+            'Units', 'normalized', 'Position', [0.55 0.9 0.45 0.1]); %#ok<NASGU>
+        use_file_check = uicontrol('Style', 'checkbox', ...
+            'Value', 1, 'String', 'Use This Data', 'Parent', panels(kk), ...
+            'Units', 'normalized', 'Position', [0 0 1 0.1]);
+        use_file_check.UserData.Data = data(:,(1+(kk-1)*3):1:(3*kk));
+        use_file_check.UserData.Vector = vector_data(:,kk);
+        chax = subplot(num_chans, 1, kk,  'Parent', tab);
+        set(panels(kk), 'UserData', chax);
+        set(tab, 'UserData', panels);
+        plot(t,data(:,(1+(kk-1)*3):1:(3*kk)))
+        axis tight; hold on
+        
+        parameter_changed(wind_size_slider);
     end
     
-    function parameter_changed(hObject, eventdata, handles)
-        slider_value = get(hObject,'Value');
+    function parameter_changed(hObject, ~, ~)
+        panel = hObject.Parent;
+        thisfigure = panel.UserData;
+        
+        ch_data = panel.Children(1).UserData.Data;
+        ch_vector = panel.Children(1).UserData.Vector;
+        
+        traffic_duration = panel.Children(4).Value;
+        traffic_threshold = panel.Children(5).Value;
+        set(panel.Children(2), 'String', num2str(traffic_duration*Ts));
+        set(panel.Children(3), 'String', num2str(traffic_threshold)); 
+       
         slider_purpose = get(hObject, 'String');
         if(strcmp(slider_purpose,'Duration'))
-            DataStruct.traffic_duration = slider_value;
-            win_size = ceil(DataStruct.traffic_duration/Ts/2)*2;
+            win_size = min(ceil(traffic_duration/2)*2, length(ch_vector)-1);
             window = bartlett(win_size+1);
-            DataStruct.th_data = apply_window(window, DataStruct.Vector);
-        else
-            DataStruct.traffic_threshold = slider_value;
+            panel.Children(5).UserData = apply_window(window, ch_vector);
         end
-        maxval = max(reshape(DataStruct.th_data,numel(DataStruct.th_data),1));
-        minval = min(reshape(DataStruct.th_data,numel(DataStruct.th_data),1));
-        if(threshold_slider.Value>maxval)
-            threshold_slider.Value = maxval;
-            DataStruct.traffic_threshold = maxval;
-        elseif(threshold_slider.Value<minval)
-            threshold_slider.Value = minval;
-            DataStruct.traffic_threshold = minval;
-        end
-        set(wind_size_readout, 'String', ...
-            ['Duration:', num2str(DataStruct.traffic_duration)]);
-        set(threshold_readout, 'String', ...
-            ['Threshold:', num2str(DataStruct.traffic_threshold)]);
-        set(threshold_slider,'Max', maxval);
-        set(threshold_slider,'Min', minval);            
-        mid = (maxval + minval)/2;
         
-        comb = rms(DataStruct.th_data, 2);
-        subplot(2,1,2, 'Parent', tab); hold off
-        plot(t, comb,'k');hold on
-        plot(t, DataStruct.th_data, '--');
-        axis([0 t(end) 0 1]); axis autoy
+        smooth_vector = panel.Children(5).UserData;
+        maxval = max(smooth_vector);
+        minval = min(smooth_vector);
+        spread = maxval-minval;
+        threshold = traffic_threshold * spread + minval;
+        margin = spread*frame_diff;
 
-        low_level = comb<DataStruct.traffic_threshold*0.9;
-        high_level = comb>DataStruct.traffic_threshold*1.1;
+        alow = threshold - margin;
+        low_level = smooth_vector<alow;
+        ahigh = threshold + margin;
+        high_level = smooth_vector>ahigh;
+
+        subplot(thisfigure); hold off
+        plot(t,ch_data,':');hold on
+        plot(t, smooth_vector,'k','LineWidth',2)
+        plot(t,low_level*(maxval-minval)+minval,'g')
+        plot(t,high_level*(maxval-minval)+minval,'m')
+        plot(t([1 end]), [1 1]*alow,'r--');
+        plot(t([1 end]), [1 1]*ahigh,'b--');
+        plot(t([1 end]), [1 1]*threshold,'k');
+        axis([0 t(end) 0 max(max(ch_data))])
         
-        plot(t,low_level*mid,'g-')
-        plot(t,high_level*mid,'m-')
-        plot(t([1 end]), [1 1]*0.9*DataStruct.traffic_threshold,'r');
-        plot(t([1 end]), [1 1]*1.1*DataStruct.traffic_threshold,'b');
-
-        overlap=0.5;
-        DataStruct.frames_Low = findFrames(low_level, frame_size, overlap);
-        DataStruct.frames_High = findFrames(high_level, frame_size, overlap);
+        frames_Low = findFrames(low_level, frame_size, overlap);
+        frames_High = findFrames(high_level, frame_size, overlap);
         count = 0;
-        for id=DataStruct.frames_Low
-            plot([id, id+frame_size]/Fs,...
-                    mid/2*[1 1]+0.1*(mod(count,3)+1),'LineWidth',3,'Color','g');
+        for id=frames_Low
+            plot(   [id, id+frame_size]/Fs,...
+                    maxval*0.75*[1 1]+spread/5*(mod(count,3)+1), ...
+                    'LineWidth',3,'Color','g');
             count = count+1;
         end
-        for id=DataStruct.frames_High
-            plot([id, id+frame_size]/Fs,...
-                    mid/2*[1 1]+0.1*(mod(count,3)+1),'LineWidth',3,'Color','m');
+        count = 0;
+        for id=frames_High
+            plot(   [id, id+frame_size]/Fs,...
+                    maxval*[1 1]+spread/3*(mod(count,3)+1), ...
+                    'LineWidth',3,'Color','m');
             count = count+1;
         end
-        set(tab, 'UserData', DataStruct);
+        panel.Children(1).UserData.Low = frames_Low;
+        panel.Children(1).UserData.High = frames_High;
     end
 end
--- calculateHVSR.m
+++ calculateHVSR.m
@@ -3,22 +3,22 @@ function HVSR = calculateHVSR(signal, frame_starts, window)
     Nch = floor(size(signal,2)/3);
     Nframes = length(frame_starts);
     HVSR = zeros(window_size/2, Nframes, Nch);
-    window = repmat(window, 1, Nch);
+%     window = repmat(window, 1, Nch);
     
 %     Fs = 125;
 %     freq = Fs*(1:window_size/2)'/window_size;
     spec_window_width = 7;
     spec_window = bartlett(spec_window_width);
 
-    for idx = frame_starts
-        windata = signal(idx:idx+window_size-1,:).*window;
-        fftdata = abs(fft(windata, window_size, 1)/window_size);
-        fftdata = fftdata(1:window_size/2,:)+fftdata(end:-1:1+window_size/2,:);
-        fftdata = apply_window(spec_window,fftdata);
-        for jdx = 1:Nch
-            H = sqrt(fftdata(:,2+(jdx-1)*3).^2 + fftdata(:,jdx*3).^2);
-            V = fftdata(:,1+(jdx-1)*3);
-            HVSR(:,frame_starts==idx,jdx) = H ./ V;
+    for ch=1:1:Nch
+        for idx = frame_starts
+            windata = signal(idx:idx+window_size-1,(3*ch-2):(3*ch)).*window;
+            fftdata = abs(fft(windata, window_size, 1)/window_size);
+            fftdata = fftdata(1:window_size/2,:)+fftdata(end:-1:1+window_size/2,:);
+            fftdata = apply_window(spec_window,fftdata);
+            H = sqrt(fftdata(:,2).^2 + fftdata(:,3).^2);
+            V = fftdata(:,1);
+            HVSR(:,frame_starts==idx,ch) = H ./ V;
         end
     end
 end
